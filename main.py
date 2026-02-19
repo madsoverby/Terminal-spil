1914210dc935a9d4b328847d46bc5969ea2f40f4
mads <mads@overbyklanen.dk>
2026-02-19 15:37:51 +0100

    laved en fil som beholder hvor mange forsø du vil ha

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import random
 
+with open("pref.txt", "r") as con:
+    defult = con.read()
 
-def main(gange_tilbgae=5):
+defult = int(defult) - 1 
+def main(gange_tilbgae=defult):
     print("gæt et tal fra 1 til 100")
     print("lidt lige som tanken brænder jeg siger om det er højere eller laver")
 
@@ -21,16 +24,23 @@ def main(gange_tilbgae=5):
             print(f"du gættede {ins} men du skal lige lidt højre")
         if ins > tal:
             print(f"du gættede {ins} men du skal lige lidt lavere")
+
         gange_tilbgae = gange_tilbgae - 1
+
         ins = int(input("prøv igen --> "))
+while True:
 
-print("hvis du ikke skriver noget gård den til spil automatisk")
-print("Indstillinger")
-print("Spil")
+    print("hvis du ikke skriver noget gård den til spil automatisk")
+    print("Indstillinger")
+    print("Spil")
 
-ins = input("Skriv her --> ")
+    ins = input("Skriv her --> ")
 
-if ins == "Indstillinger":
-    main(int(input("hvormaneg gange skal du have -->")))
-else:
-    main()
+    if ins == "Indstillinger":
+        input = input("hvor mange gange skal du have hver gang --> ") 
+        with open("pref.txt", "w") as con:
+            con.write(input)
+
+    else:
+        main()
+        break

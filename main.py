1821286f33e719356c13b9e293e077a3e319d57f
mads <mads@overbyklanen.dk>
2026-02-19 16:50:40 +0100

    tilføjet try og exept

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import random
+try:
+    with open("pref.txt", "r") as con:
+        defult = con.read()
+except:
+    with open("pref.txt", "w") as con:
+        con.write("7")
+        defult = 7
 
-with open("pref.txt", "r") as con:
-    defult = con.read()
 
-defult = int(defult) - 1 
-def main(gange_tilbgae=defult):
+defult = int(defult)
+
+def main(gange_tilbage=defult):
     print("gæt et tal fra 1 til 100")
     print("lidt lige som tanken brænder jeg siger om det er højere eller laver")
-
-
-    ins = int(input("du skal bare skrive dit tal lige her --> "))
 
     tal = random.randint(1, 100)
 
     while True:
+        try:
+            ins = int(input("du skal bare skrive dit tal lige her --> "))    
+        except ValueError:
+            print("Du må kun skrive tal")
+            continue
         if ins == tal:
             print(f"du  klarde det du gættede nemelig {tal}")
             break
-        if gange_tilbgae == 0:
+        if gange_tilbage == 1:
             print(f"du havde ikke flere gange tilbage tallet var {tal}")
             break
         if ins < tal:
@@ -25,25 +33,21 @@ def main(gange_tilbgae=defult):
         if ins > tal:
             print(f"du gættede {ins} men du skal lige lidt lavere")
 
-        gange_tilbgae = gange_tilbgae - 1
+        gange_tilbage = gange_tilbage - 1
 
-        ins = int(input("prøv igen --> "))
-    
-while True:
+print("hvis du ikke skriver noget går den til spil automatisk med 7 førsøg vis du vil ændre så gå ind i indstillinger")
+print("Indstillinger")
+print("Spil")
 
-    print("hvis du ikke skriver noget gård den til spil automatisk")
-    print("Indstillinger")
-    print("Spil")
+ins = input("Skriv her --> ")
 
-    ins = input("Skriv her --> ")
+if ins == "Indstillinger":
+    inputs = input("hvor mange gange skal du have hver gang --> ") 
+    with open("pref.txt", "w") as con:
+        con.write(inputs)
+    main(int(inputs))
+        
 
-    if ins == "Indstillinger":
-        inputs = input("hvor mange gange skal du have hver gang --> ") 
-        with open("pref.txt", "w") as con:
-            con.write(inputs)
-        main()
-        break
-
-    else:
-        main()
-        break
+else:
+     main()
+       
